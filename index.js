ba51b01b18a53ffc2df561f034fc7c8553f71a47
sudheerneo <mailtobsudheer@gmail.com>
2023-03-28 14:12:31 +0530

    added mailjs

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 $(document).ready(() => {
     // disable scrolling until loader complete task with with css
     $("body").addClass("stopScrolling");
-
+    function recaptchaCallback() {
+        $("#sendMessageButton").removeAttr("disabled");
+    }
     // hides loader after 2 sec
     setTimeout(() => {
         $("#loader").hide();
@@ -10,4 +12,14 @@ $(document).ready(() => {
             $(".toast").toast("show");
         }, 4000);
     }, 2000);
+    $("#test").click(() => {
+        console.log("sending mail");
+        // code fragment
+        // emailjs.send("service_hxc0svr", "template_gpb89th", {
+        //     from_name: "Sudheer Website",
+        //     to_name: "Sudheer",
+        //     message: "Hello",
+        // });
+        console.log("mail req sent");
+    });
 });
