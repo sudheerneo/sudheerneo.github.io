f24f7eacfc72b2eaa68e504e93bf635e7b689a27
sudheerneo <mailtobsudheer@gmail.com>
2023-04-04 01:20:43 +0530

    added fullscreen feature

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ $(document).ready(() => {
     // disable scrolling until loader complete task with with css
     $("body").addClass("stopScrolling");
 
+    // go fullscreen
+    $(".fulscreenBtn").click(() => {
+        goFullscreen();
+        $(".toast").toast("hide");
+    });
+    $(document).keyup((e) => {
+        e.key.toLowerCase() === "enter" ? goFullscreen() : 0;
+    });
     // hides loader after 2 sec
     setTimeout(() => {
         $("#loader").hide();
@@ -14,6 +22,20 @@ $(document).ready(() => {
         }, 4000);
     }, 2000);
 });
+
+const goFullscreen = () => {
+    const body = document.documentElement;
+    body.requestFullScreen
+        ? body.requestFullscreen()
+        : body.webkitRequestFullscreen
+        ? body.webkitRequestFullscreen()
+        : body.msRequestFullscreen
+        ? body.msRequestFullscreen()
+        : body.mozRequestFullscreen
+        ? body.mozRequestFullscreen()
+        : 0;
+};
+
 function recaptchaCallback(response) {
     $("#capctchaResToken").val(response);
     $("#sendMessageButton").removeAttr("disabled");
